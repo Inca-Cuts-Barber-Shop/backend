46ef7dcba4ec890a3bf37ddeec1666cac207969a
Joseph Camarena <pepecamarena5@gmail.com>
2024-01-23 18:27:02 -0700

    "Initial commit with Spring Boot project structure / dependancys"

---
+++ incaserver/src/main/java/com/incacuts/incaserver/IncaserverApplication.java
@@ -0,0 +1,13 @@
+package com.incacuts.incaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncaserverApplication.class, args);
+	}
+
+}
---
+++ incaserver/src/test/java/com/incacuts/incaserver/IncaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incacuts.incaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
